8e605189fe543df20cc6857f605926e24db1b52c
Federico <crudofederico@gmail.com>
2020-12-14 19:12:37 +0100

    update normal

--- js/script.js
+++ js/script.js
@@ -1,28 +1,20 @@
 
-function biglietti(){
-    var km=document.getElementById('km');
-    var eta= document.getElementById('eta') ;
+    var km=prompt("KM da Percorrere");
+    var eta= prompt("Età passegero") ;
     var costo=0.21;
-    var pbase=km.value*costo;
+    var pbase=km*costo;
     var sconto20=Math.round(pbase*0.20);
     var sconto40=Math.round(pbase*0.40);
+    document.getElementById('km').value= km; 
+    document.getElementById('eta').value= eta; 
+    console.log(km);
+    console.log(pbase);
     //controllo campi
-    if(km.value==""|| eta.value==""){
-        document.getElementById('messageb').innerHTML= '<span id="text"><span></i>';
-        document.getElementById('text').innerHTML= '* Si prega di compilare tutti i campi';
-        document.getElementById('result2').innerHTML= '' ;
-    }
-    else {  
-        document.getElementById('messageb').innerHTML= '';
-        // minorenni
-        if((eta.value>0)&&(eta.value<18)){   
+        if((eta>0)&&(eta<18)){   
             document.getElementById('result2').innerHTML= 'Il prezzo del biglietto è di: '+(pbase-sconto20)+'€'+ ' '+'(Sconto 20% applicato)'; 
-            
         }
         // over 65
-        else if(eta.value>65){
-            console.log(sconto20);
-            console.log(pbase);
+        else if(eta>65){
             document.getElementById('result2').innerHTML= 'Il prezzo del biglietto è di: '+(pbase-sconto40)+'€'+ ' '+'(Sconto 20% applicato)'; 
         }
         // dai 17 ai 68
@@ -32,10 +24,9 @@ function biglietti(){
             
         
  
-    }
+    
         
           
    
 
     
-}
